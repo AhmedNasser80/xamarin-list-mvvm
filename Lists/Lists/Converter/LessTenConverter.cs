f46a73d417e14927829c10444cefb04c27997afe
AhmedNasser80 <ahnma600@gmail.com>
2022-08-29 03:49:44 +0200

    theme and convert

---
+++ Lists/Lists/Converter/LessTenConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Lists.Converter
+{
+    public class LessTenConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int qunataty = 0;
+            int disNum = (int)value;
+            if (disNum < 10)
+            {
+                qunataty = 15;
+            }
+            return qunataty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
